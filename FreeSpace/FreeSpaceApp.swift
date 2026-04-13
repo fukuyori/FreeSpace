d1a67739374acb675bb41ef9708699e01ed1d34a
fukuyori <self@spumoni.org>
2026-04-14 02:32:14 +0900

    Initial Commit

---
+++ FreeSpace/FreeSpaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  FreeSpaceApp.swift
+//  FreeSpace
+//
+//  Created by 福寄典明 on 2026/04/14.
+//
+
+import SwiftUI
+
+@main
+struct FreeSpaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
